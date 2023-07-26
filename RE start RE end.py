c8bcc96ed8c876bf0e16358b2e61771a8397dad3
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-07-26 22:24:11 +0530

    Add files via upload

---
+++ RE start RE end.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+string = input()
+substring = input()
+
+pattern = re.compile(substring)
+match = pattern.search(string)
+if not match: print('(-1, -1)')
+while match:
+    print('({0}, {1})'.format(match.start(), match.end() - 1))
+    match = pattern.search(string, match.start() + 1)
