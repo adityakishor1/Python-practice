befcd89b893aa2ed29a87cd81be400fa52faed81
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-07-31 13:30:27 +0530

    Add files via upload

---
+++ 2 array operation.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def arrayOperations(self, n : int, arr : List[int]) -> int:
+    
+        cnt=0
+        flag=False
+        allNonZero=True
+        for i in range(n):
+            if(arr[i]!=0):
+                if(not flag): cnt+=1
+                flag=True
+            else:
+                allNonZero=False
+                flag=False
+        
+        if(allNonZero):
+            return -1
+            
+        return cnt
