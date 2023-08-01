d127ebfa64a968c88e680043a4399a0b2e1ba4c0
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-08-01 12:31:59 +0530

    Add files via upload

---
+++ frist 10 natural no..py
@@ -0,0 +1,29 @@
+for i in range(10):
+	print(i+1)
+# second method	
+print(" ")
+print("#"*30)	
+x=1
+while x<=10:
+     print(x, end=" ")
+     x+=1
+
+ # third method
+print(" " and "#"*20)
+print([i for i in range(1,11)])    
+#second program
+print(" ")
+print("#"*30)
+for i in range(1,20,2):
+	print(i)
+# Program of reverse of natural number
+print(" ")
+print("*"*35)
+for i in range(10,0,-1):
+	print(i)
+#second method
+print(" " and "#"*20)
+x=10
+while x>=1:
+	print(x)
+	x-=1
