cafefcd3f469f8dc7139e7becbc23d2d3c097711
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-11-10 14:44:58 +0530

    Add files via upload

---
+++ Contain dublicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        
+        for num in nums:
+            if num in seen: return True
+            seen.add(num)
+
+        return False
