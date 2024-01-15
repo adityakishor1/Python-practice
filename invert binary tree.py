8b5366f527beedad319dfd08360e9d50f3363ae6
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-01-15 12:35:01 +0530

    Add files via upload

---
+++ invert binary tree.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def invertTree(self, root):
+        if not root: return root
+        q = collections.deque([root])
+        
+        while q:
+            node = q.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left: q.append(node.left)
+            if node.right: q.append(node.right)
+        
+        return root
