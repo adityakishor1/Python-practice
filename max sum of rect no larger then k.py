b43edee4d6c1ec65e017d9cbda23c07537a85c60
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2024-02-22 23:12:04 +0530

    Add files via upload

---
+++ max sum of rect no larger then k.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def maxSumSubmatrix(self, matrix, k):
+        if len(matrix)>len(matrix[0]): matrix = self.rotate(matrix)
+        
+        ans = float('-inf')
+        M = len(matrix)
+        N = len(matrix[0])
+        
+        for start in xrange(M):
+            rowSum = [0]*N #row sum of rows from matrix[start] to row
+            for row in matrix[start:]:
+                for i, n in enumerate(row): rowSum[i] += n
+                ans = max(ans, self.maxSumRow(rowSum, k))
+                if ans==k: return ans
+        return ans
+    
+    def maxSumRow(self, row, k):
+        ans = float('-inf')
+        total = 0
+        
+        ss = SortedSet()
+        ss.add(0)
+        
+        for n in row:
+            total += n
+            i = ss.bisect_left(total-k)
+            if i<len(ss):
+                x = ss[i]
+                ans = max(ans, total-x)
+            ss.add(total)
+        return ans
+            
+    def rotate(self, matrix):
+        M = len(matrix)
+        N = len(matrix[0])
+        matrixRotated = [[0]*M for _ in xrange(N)]
+        
+        for i in xrange(M):
+            for j in xrange(N):
+                matrixRotated[j][i] = matrix[i][j]
+        
+        return matrixRotated
+            
