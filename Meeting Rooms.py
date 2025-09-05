9f20534eec2716a1ba9571961910ddadaf53353d
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2025-09-05 21:15:42 +0530

    Add files via upload

--- Meeting Rooms.py
+++ Meeting Rooms.py
@@ -1,7 +1,3 @@
-class Solution:
-    def canAttend(self, arr):
-        arr.sort(key=lambda x: x[0])
-        for i in range(1, len(arr))
-            if arr[i][0] < arr[i - 1][1]:
-                return False 
-        return True
+a= "masala"
+for letter in a:
+    print(letter)
