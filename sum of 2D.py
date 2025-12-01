6dec777c75ee3e77945878706cf83179ee51db8f
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2025-12-01 19:38:22 +0530

    Add files via upload

--- sum of 2D.py
+++ sum of 2D.py
@@ -1,14 +1,10 @@
-matrix=[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-rows= len(matrix)
-cols= len(matrix[0])
-for j in range(cols):
-    col_sum=0
-    for i in range(rows):
-        col_sum += matrix[i][j]
-    print(f"Sum of column {j}: {col_sum}")
-
-    
+a=int(input("Enter your numer"))
+if a>1:
+    for i in range(2, int(a**0.5)+1):
+        if a % i==0:
+            print("not prime")
+            break
+    else:
+        print("prime")
+else:
+    ("Not prime")
